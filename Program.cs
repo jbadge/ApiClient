8baede6db419dd1500ed75c39452e6c47b307915
Jeremy Badger <jeremebadger@gmail.com>
2023-05-09 16:29:00 -0700

    Access Jokes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace ApiClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var responseBodyAsStream = await client.GetStreamAsync("https://official-joke-api.appspot.com/random_joke");
+
+            var item = await JsonSerializer.DeserializeAsync<Item>(responseBodyAsStream);
+
+            Console.Clear();
+            Console.WriteLine($"{item.Setup}\n\n{item.Punchline}");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true).Key.ToString();
+            Console.Clear();
+
+        }
     }
-  }
 }
